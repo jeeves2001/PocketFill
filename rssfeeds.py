3c5048cffa406cffb148267f228e1c567099851d
jeeves2001 <james_stevens2001@hotmail.com>
2018-03-13 14:32:47 +0000

    Update rssfeeds.py

--- rssfeeds.py
+++ rssfeeds.py
@@ -4,5 +4,8 @@ my_feeds = [
     'http://feeds.feedburner.com/TechCrunch/',
     'https://www.theverge.com/rss/index.xml',
     'http://feeds.bbci.co.uk/news/technology/rss.xml?edition=uk#',
-    'https://www.engadget.com/rss.xml'
+    'https://www.engadget.com/rss.xml',
+    'http://feeds.bbci.co.uk/news/rss.xml?edition=uk',
+    'https://www.theguardian.com/uk/rss',
+    'https://www.theregister.co.uk/headlines.atom'
 ]
